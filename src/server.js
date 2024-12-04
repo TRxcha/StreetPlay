117ba3d83b9301c61d13a9d11fcc93c732d1f06a
lucas <lucasphelipe280@gmail.com>
2024-12-03 22:24:57 -0300

    commit tcc1

---
+++ src/server.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+app.post('/', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get('/', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.status(200).json(users)
+})
+
+app.put('/:id', async (req, res) => {
+    const users = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(200).json(users)
+    })
+
+app.delete('/:id', async(req, res) => {
+    const users = await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message: 'Usuário deletado com sucesso'})
+})
+
+app.listen(7777)
